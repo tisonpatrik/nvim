804d5d399ee3489fe3dd44ca313bed618c22d9e7
tisonpatrik <patriktison@gmail.com>
2025-02-22 13:50:22 +0100

    formater was fixed

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,6 +5,16 @@ return {
 		lint.linters_by_ft = {
 			lua = { "luacheck" },
 		}
+		lint.linters.luacheck.args = {
+			"--globals",
+			"love",
+			"vim",
+			"--formatter",
+			"plain",
+			"--codes",
+			"--ranges",
+			"-",
+		}
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			callback = function()
 				lint.try_lint()
