de6f22928466db245e5d55e38ac86ca04f3ad706
tisonpatrik <patriktison@gmail.com>
2025-08-01 21:35:10 +0000

    docs view was added

---
+++ docs_view.lua
@@ -0,0 +1,9 @@
+return {
+	"amrbashir/nvim-docs-view",
+	cmd = "DocsViewToggle",
+	lazy = true,
+	opts = {
+		position = "right",
+		width = 60,
+	},
+}
--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,31 +1,10 @@
 return {
 	"goolord/alpha-nvim",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons",
+		"echasnovski/mini.icons",
+		"nvim-lua/plenary.nvim",
 	},
-
 	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
-
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
-
-		alpha.setup(dashboard.opts)
+		require("alpha").setup(require("alpha.themes.theta").config)
 	end,
 }
